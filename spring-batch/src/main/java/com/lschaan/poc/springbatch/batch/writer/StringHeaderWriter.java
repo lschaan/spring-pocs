cbbf4e9599cdb50c913a21026c4b140f09778ca2
Lucas Schaan <ilegra.schaan@agibank.com.br>
2019-11-12 08:57:58 -0300

    refactor: construtor usando lombok

--- spring-batch/src/main/java/com/lschaan/poc/springbatch/batch/writer/StringHeaderWriter.java
+++ spring-batch/src/main/java/com/lschaan/poc/springbatch/batch/writer/StringHeaderWriter.java
@@ -2,15 +2,13 @@ package com.lschaan.poc.springbatch.batch.writer;
 
 import java.io.IOException;
 import java.io.Writer;
+import lombok.AllArgsConstructor;
 import org.springframework.batch.item.file.FlatFileHeaderCallback;
 
+@AllArgsConstructor
 public class StringHeaderWriter implements FlatFileHeaderCallback {
 
   private final String header;
-
-  StringHeaderWriter(String header) {
-    this.header = header;
-  }
 
   @Override
   public void writeHeader(Writer writer) throws IOException {
