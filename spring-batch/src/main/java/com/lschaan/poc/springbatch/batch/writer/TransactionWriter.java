2368d695a1a8b816c44ab0c056157dbba6481be9
Lucas Schaan <ilegra.schaan@agibank.com.br>
2019-11-12 13:37:11 -0300

    refactor: ItemWriter

--- spring-batch/src/main/java/com/lschaan/poc/springbatch/batch/writer/TransactionWriter.java
+++ spring-batch/src/main/java/com/lschaan/poc/springbatch/batch/writer/TransactionWriter.java
@@ -1,41 +1,29 @@
 package com.lschaan.poc.springbatch.batch.writer;
 
 import com.lschaan.poc.springbatch.dto.TransactionDTO;
-import org.springframework.batch.item.ItemWriter;
+import javax.annotation.PostConstruct;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.FieldExtractor;
 import org.springframework.batch.item.file.transform.LineAggregator;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Component;
 
-@Configuration
-public class TransactionWriter {
+@Component
+public class TransactionWriter extends FlatFileItemWriter<TransactionDTO> {
 
-  @Bean
-  public ItemWriter<TransactionDTO> transactionDTOItemWriter() {
-    FlatFileItemWriter<TransactionDTO> csvFileWriter = new FlatFileItemWriter<>();
-
-    String exportHeader = "ID;CPF;DESCRIPTION";
-    StringHeaderWriter headerWriter = new StringHeaderWriter(exportHeader);
-    csvFileWriter.setHeaderCallback(headerWriter);
-
-    LineAggregator<TransactionDTO> lineAggregator = createLineAggregator();
-    csvFileWriter.setLineAggregator(lineAggregator);
-    csvFileWriter.setResource(new FileSystemResource("transaction.csv"));
-
-    return csvFileWriter;
+  @PostConstruct
+  public void init() {
+    setHeaderCallback(new StringHeaderWriter("ID;CPF;DESCRIPTION"));
+    setResource(new FileSystemResource("transaction.csv"));
+    setLineAggregator(createLineAggregator());
   }
 
   private LineAggregator<TransactionDTO> createLineAggregator() {
     DelimitedLineAggregator<TransactionDTO> lineAggregator = new DelimitedLineAggregator<>();
     lineAggregator.setDelimiter(";");
-
-    FieldExtractor<TransactionDTO> extractor = createExtractor();
-    lineAggregator.setFieldExtractor(extractor);
-
+    lineAggregator.setFieldExtractor(createExtractor());
     return lineAggregator;
   }
 
