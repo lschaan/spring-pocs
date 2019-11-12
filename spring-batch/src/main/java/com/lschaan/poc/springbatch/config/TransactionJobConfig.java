a4cfb68314766c167457b451c74c09b854d1cbba
Lucas Schaan <ilegra.schaan@agibank.com.br>
2019-11-12 09:00:07 -0300

    refactor: imports desnecessários

--- spring-batch/src/main/java/com/lschaan/poc/springbatch/config/TransactionJobConfig.java
+++ spring-batch/src/main/java/com/lschaan/poc/springbatch/config/TransactionJobConfig.java
@@ -1,7 +1,6 @@
 package com.lschaan.poc.springbatch.config;
 
 import com.lschaan.poc.springbatch.dto.TransactionDTO;
-import javax.sql.DataSource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -12,7 +11,6 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
